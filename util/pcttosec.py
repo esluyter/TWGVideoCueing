d39693259c8fab2fb6f91b1cfa622096d36c2036
Eric Sluyter <ericsluyter@Finnegan.local>
2019-02-24 11:43:02 -0500

    Utility for converting positions to time

---
+++ util/pcttosec.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3
+import math
+
+media_length = 22 * 60 + 58
+
+while True:
+    position = input('Position (%) or "Q" to quit: ')
+    if position.upper() == 'Q':
+        break
+    try:
+        position = float(position)
+    except:
+        print('Invalid position number, try again.')
+    else:
+        seconds = position * 0.01 * media_length
+        minutes = math.floor(seconds / 60)
+        seconds = seconds - (minutes * 60)
+        print(position, '%  => ', minutes, 'minutes and', round(seconds, 2), 'seconds')
